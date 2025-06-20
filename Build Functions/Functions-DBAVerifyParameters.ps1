01aa6fe081c908ed0d584c549762e0d33f027c60
hugostyles <41150642+hugostyles@users.noreply.github.com>
2025-06-20 16:49:52 +0100

    set

--- Build Functions/Functions-DBAVerifyParameters.ps1
+++ Build Functions/Functions-DBAVerifyParameters.ps1
@@ -1,20 +1,46 @@
 param(
-    [string]$SQLServers
+    [string]$SQLServers,
+    [string]$Authority
 )
 
 function Invoke-VerifyParameters {
     param(
-        [string]$SQLServers
+        [string]$SQLServers,
+        [string]$Authority
     )
 
+    write-host " " 
+    Write-host "Please review the below parameters:"
+    write-host '----------------------------------------'
+    Write-host "Authority : " $Authority
     Write-host "Servers : " $SQLServers
+    Write-host "EnvironmentName : " $EnvironmentName
+    Write-host "EnvironmentType : " $EnvironmentType
+    Write-host "EnvironmentCategory : " $EnvironmentCategory
+    Write-host "EnvironmentPrimary : " $EnvironmentPrimary
+    Write-host "ReadableSecondaryLocation : " $ReadableSecondaryLocation
+    Write-host "SQLServerBuild : " $SQLServerBuild
+    Write-host "EMISGroupDBAVersion : " $EMISGroupDBAVersion
+    Write-host "EMISServiceManagerVersion : " $EMISServiceManagerVersion
+    Write-host "DeployEMISServiceManager : " $DeployEMISServiceManager
+    Write-host "IsAWS : " $IsAWS
+    Write-host "Reporting Server : " $ReportingServer
+    Write-host "SentryOne Server : " $SentryOneDB
+    write-host "Service : " $Service
+    write-host '----------------------------------------'
+    
+    #start with no errors
     $haserror = 0
 
+    #check if the parameters are set
     if ( ([string]::IsNullOrEmpty($SQLServers))){
         Write-Output '$SQLServers - Missing SQLServers Parameter'
         $haserror = 1}
-    
-    
+    if ( ([string]::IsNullOrEmpty($Authority))){
+        Write-Output '$Authority - Missing Authority Parameter'
+        $haserror = 1}
+
+        
     #Summary of the checks
     if ($haserror -ne '1')
     {
