8e99cea9010707eaf501b9311364383c98373334
hugostyles <41150642+hugostyles@users.noreply.github.com>
2025-06-26 15:54:12 +0100

    parameters

--- Build Functions/Functions-DBAVerifyParameters.ps1
+++ Build Functions/Functions-DBAVerifyParameters.ps1
@@ -1,43 +1,75 @@
 param(
-    [string]$SQLServers,
-    [string]$Authority
-)
-
-function Invoke-VerifyParameters {
-    param(
-        [string]$SQLServers,
-        [string]$Authority
+        [string]$Servernames,
+        [string]$Authority,
+        [string]$SQLServerBuild,
+        [string]$EnvironmentName,
+        [string]$EnvironmentType,
+        [string]$EnvironmentCategory,
+        [string]$EMISGroupDBAVersion,
+        [string]$EMISServiceManagerVersion,
+        [string]$ReportingServer,
+        [string]$SentryOneDB
     )
 
     write-host " " 
     Write-host "Please review the below parameters:"
     write-host '----------------------------------------'
+    Write-host "Servers : " $Servernames
     Write-host "Authority : " $Authority
-    Write-host "Servers : " $SQLServers
+    Write-host "SQLServerBuild : " $SQLServerBuild
     Write-host "EnvironmentName : " $EnvironmentName
     Write-host "EnvironmentType : " $EnvironmentType
     Write-host "EnvironmentCategory : " $EnvironmentCategory
-    Write-host "EnvironmentPrimary : " $EnvironmentPrimary
-    Write-host "ReadableSecondaryLocation : " $ReadableSecondaryLocation
-    Write-host "SQLServerBuild : " $SQLServerBuild
     Write-host "EMISGroupDBAVersion : " $EMISGroupDBAVersion
     Write-host "EMISServiceManagerVersion : " $EMISServiceManagerVersion
-    Write-host "DeployEMISServiceManager : " $DeployEMISServiceManager
-    Write-host "IsAWS : " $IsAWS
     Write-host "Reporting Server : " $ReportingServer
     Write-host "SentryOne Server : " $SentryOneDB
-    write-host "Service : " $Service
     write-host '----------------------------------------'
     
     #start with no errors
     $haserror = 0
 
     #check if the parameters are set
-    if ( ([string]::IsNullOrEmpty($SQLServers))){
-        Write-Output '$SQLServers - Missing SQLServers Parameter'
+    if ( ([string]::IsNullOrEmpty($Servernames))){
+        Write-Output '$Servernames - Missing Servernames Parameter'
         $haserror = 1}
+
     if ( ([string]::IsNullOrEmpty($Authority))){
         Write-Output '$Authority - Missing Authority Parameter'
+        $haserror = 1}
+
+    if ( ([string]::IsNullOrEmpty($SQLServerBuild))){
+        Write-Output '$SQLServerBuild - Missing SQLServerBuild Parameter'
+        $haserror = 1}
+    
+    if ( ([string]::IsNullOrEmpty($EnvironmentName))){
+        Write-Output '$EnvironmentName - Missing EnvironmentName Parameter'
+        $haserror = 1}
+    
+    if ( ([string]::IsNullOrEmpty($EnvironmentType))){
+        Write-Output '$EnvironmentType - Missing EnvironmentType Parameter'
+        $haserror = 1} elseif ($EnvironmentType -notin @('PRD','STG','DEV')) {
+        Write-Output "$EnvironmentType - Invalid EnvironmentType Parameter. Must be PRD, STG or DEV."
+        $haserror = 1}
+    
+    if ( ([string]::IsNullOrEmpty($EnvironmentCategory))){
+        Write-Output '$EnvironmentCategory - Missing EnvironmentCategory Parameter'
+        $haserror = 1}
+
+    if ( ([string]::IsNullOrEmpty($EMISGroupDBAVersion))){    
+        Write-Output '$EMISGroupDBAVersion - Missing EMISGroupDBAVersion Parameter'
+        $haserror = 1}
+
+    if ( ([string]::IsNullOrEmpty($EMISServiceManagerVersion))){ 
+        Write-Output '$EMISServiceManagerVersion - Missing EMISServiceManagerVersion Parameter'
+        $haserror = 1}
+
+    if ( ([string]::IsNullOrEmpty($ReportingServer))){
+        Write-Output '$ReportingServer - Missing ReportingServer Parameter'
+        $haserror = 1}  
+
+    if ( ([string]::IsNullOrEmpty($SentryOneDB))){
+        Write-Output '$SentryOneDB - Missing SentryOneDB Parameter'
         $haserror = 1}
 
 
@@ -55,8 +87,3 @@ function Invoke-VerifyParameters {
         Write-Output "Exiting script with failure status."
         exit 1
     }
-   
-}
-
-
-Invoke-VerifyParameters -SQLServers $SQLServers -Authority $Authority
